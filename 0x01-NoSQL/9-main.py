76e6ee941e194fd428e2a7f312c4f385a8ee936b
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-16 18:56:36 -0500

    task 10

--- 0x01-NoSQL/9-main.py
+++ 0x01-NoSQL/9-main.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     school_collection = client.my_db.school
     new_school_id = insert_school(school_collection, name="UCSF", address="505 Parnassus Ave")
+    
     print("New school created: {}".format(new_school_id))
 
     schools = list_all(school_collection)
