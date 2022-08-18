b9ad499b6d809e039cc021eff09773b4daf3d58b
Your Name <you@example.com>
2022-08-18 02:25:46 +0000

    task 2 y 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 from uuid import uuid4
 from typing import Union
 import redis
+from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
@@ -29,6 +30,7 @@ class Cache:
 
         self._redis = redis.Redis()
         self._redis.flushdb()
+
 
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
